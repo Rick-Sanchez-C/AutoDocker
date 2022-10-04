3703ed42675a58b85323042958eeb951b0f1f364
VictorRuizMendez <114165232+VictorRuizMendez@users.noreply.github.com>
2022-10-04 11:37:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ os.system('cat ASCII.txt')
 image = 'httpd'
 binding = {80: 8080}
 
-print("Downloading Image...")
+print("Downloading Image..")
 infraestructura.pullimage(image)
 time.sleep(3)
 print("Image downloaded")
