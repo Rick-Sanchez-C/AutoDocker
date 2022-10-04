d351653143bba796d9fd3b40670880f7dd0fc1ce
RickSanchezC <87280286+Rick-Sanchez-C@users.noreply.github.com>
2022-10-04 08:34:40 +0200

    API AND EXAMPLE

--- infraestructura.py
+++ infraestructura.py
@@ -1,189 +1,44 @@
-from io import BytesIO
 import docker
-import os
+
+client = docker.from_env()
 
 
-class imagen:
-    def __init__(self):
-        self.client = docker.from_env(version="auto")
-
-    def _exists(self, tag):
-        for c in self.client.images.list():
-            if c.attrs['RepoTags']:
-                if tag == c.attrs['RepoTags'][0]:
-                    return c.attrs['RepoTags'][0]
-
-    def _login(self, username, password):
-        img = self.client.login(username=username, password=password)
-        return img
-
-    def _list_all(self):
-        print
-        ""
-        print
-        "{:15} {:30} {:20}".format('ID', 'TAG', 'SIZE MB')
-        for c in self.client.images.list():
-            if c.attrs['RepoTags']:
-                print
-                "{:15} {:30} {:20}".format( \
-                    c.attrs['Id'].split(':')[1][:12], \
-                    c.attrs['RepoTags'][0], \
-                    str(c.attrs['Size'] / 1000000)[:20])
-
-    def _create(self, dockerfile, tag):
-        parent_dir = os.path.dirname(os.path.realpath(__file__))
-        response = [line for line in \
-                    self.client.images.build(path=parent_dir, \
-                                             dockerfile=dockerfile, rm=True, tag=tag)]
-        for r in response[1]:
-            for key, value in r.iteritems():
-                print
-                str(value)
-
-    def _pull(self, tag):
-        img = self.client.images.pull(tag)
-        return img
+def runcontainer(image):
+    container = client.containers.run(image, detach=True)
+    print("Container started with ID: {}".format(container.id))
 
 
-class red:
-    def __init__(self, item):
-        self.client = docker.from_env(version="auto")
-        self.nombre = item['name']
-        self.item = item
-        if not self._exists():
-            self.create()
-
-    def _exists(self):
-        c = self.client.networks.list()
-        for n in c:
-            if n.name == self.nombre:
-                return n.name
-        return None
-
-    def create(self):
-        if self.client:
-            self.client.networks.create( \
-                self.nombre, driver=self.item['driver'])
-
-    def get_nombre(self):
-        if self.client:
-            return self.nombre
-
-    def _list_all(self):
-        c = self.client.networks.list()
-        print
-        ""
-        print
-        "{:30}".format("NOMBRE DE RED")
-        for n in c:
-            print(n.name)
+def runcontainerwithport(image, binding):
+    container = client.containers.run(image, detach=True, ports=binding)
+    print("Container started with ID: {}".format(container.id))
 
 
-class contenedor:
-    def __init__(self, item):
-        self.client = docker.from_env(version="auto")
-        self.nombre = item['name']
-        self.item = item
+def listcontainers():
+    for container in client.containers.list():
+        print(container.id)
 
-    def __exists(self):
-        ret = None
-        for c in self.client.containers.list(all=True):
-            if self.nombre == c.attrs['Name'].split('/')[1]:
-                ret = c
-                break
-        return ret
 
-    def stop(self):
-        self.c = self.__exists()
-        if self.c:
-            self.client.containers.model.stop(self.c)
+def stopall():
+    for container in client.containers.list():
+        container.stop()
 
-    def start(self):
-        self.c = self.__exists()
-        if self.c:
-            self.client.containers.model.start(self.c)
 
-    def status(self):
-        self.c = self.__exists()
-        if self.c:
-            return self.c.status.upper()
-        else:
-            return None
+def printlogs(container_id):
+    container = client.containers.get(container_id)
+    print(container.logs())
 
-    def remove(self):
-        self.c = self.__exists()
-        if self.c:
-            self.client.containers.model.remove(self.c)
-            print
-            ""
-            print
-            "Contenedor", \
-            self.c.attrs['Name'].split('/')[1], "removido"
-            print
-            ""
 
-    def info(self):
-        self.c = self.__exists()
-        if self.c:
-            print
-            " "
-            print
-            "{:15} {:20} {:15} {:30}".format( \
-                'ID', 'NOMBRE', 'ESTADO', 'IMAGEN')
-            print
-            "{:15} {:20} {:15} {:30}".format( \
-                str(self.c.attrs['Id'])[:12], \
-                self.c.attrs['Name'].split('/')[1], \
-                self.c.attrs['State']['Status'], \
-                self.c.attrs['Config']['Image'])
+def listimages():
+    for image in client.images.list():
+        print(image.id)
 
-    def create(self):
-        self.c = self.__exists()
-        if not self.c:
-            self.client.containers.create( \
-                image=self.item['image'], \
-                name=self.item['name'], \
-                ports=self.item['ports'], \
-                links=self.item['links'], \
-                environment=self.item['environment'], \
-                volumes=self.item['volumes'], \
-                entrypoint=self.item['entrypoint'], \
-                cap_add=self.item['cap_add'], \
-                mac_address=self.item['mac_address'], \
-                network=self.item['network'], \
-                detach=True, \
-                tty=True, \
-                init=True)
 
-    def execute(self, command, pathdir):
-        self.c = self.__exists()
-        if self.c:
-            if self.c.attrs['State']['Status'] == 'running':
-                return self.client.containers.model. \
-                    exec_run(self.c, cmd=command, tty=True, \
-                             workdir=pathdir)
-            else:
-                return " El contenedor ", \
-                       self.c.attrs['Name'].split('/')[1], \
-                       " no esta en ejecucion. "
+def pullimage(imagename):
+    image = client.images.pull(imagename)
+    print(image.id)
 
-    def put_file(self, dir, datos):
-        self.c = self.__exists()
-        if self.c:
-            if self.c.attrs['State']['Status'] == 'running':
-                self.client.containers.model. \
-                    put_archive(self.c, path=dir, data=datos)
-            else:
-                print
-                ""
-                print
-                " El contenedor ", self.c.attrs['Name'].split('/')[1], \
-                " no esta en ejecucion. "
 
-    def logs(self):
-        self.c = self.__exists()
-        if self.c:
-            if self.c.logs():
-                for line in self.c.logs(stream=True):
-                    print
-                    line.strip()
+def commitcontainer(container_id, commitname):
+    container = client.containers.get(container_id)
+    image = container.commit(commitname)
+    print(image.id)
--- main.py
+++ main.py
@@ -1,46 +1,16 @@
-from infraestructura import contenedor, red, imagen
+import time
+
+import infraestructura
 import os
 
+image = 'httpd'
+binding = {80: 8080}
 
-parent_dir = os.path.dirname(os.path.realpath(__file__))
-html_dir = '{}/html'.format(parent_dir)
-
-img = imagen()
-
-print("Descargando imagen, espere unos minutos....")
-img_APP = img._pull('php:7.2-apache')
-print(img_APP)
-
-INFRAESTRUCTURA = \
-    {'image': 'php:7.2-apache',
-     'name': 'contenedor_php',
-     'ports': {'80/tcp': 80},
-     'links': {},
-     'entrypoint': '',
-     'environment': [],
-     'cap_add': [],
-     'network': '',
-     'mac_address': '00:00:00:00:00:03',
-     'volumes': {html_dir: \
-                     {'bind': '/var/www/html', 'mode': 'rw'}}
-     }
-
-print("Para y remueve contenedor ...")
-ctr = contenedor(INFRAESTRUCTURA)
-ctr.stop()
-ctr.remove()
-
-print("Reinicia contenedor, espere unos minutos...")
-ctr = contenedor(INFRAESTRUCTURA)
-ctr.create()
-while True:
-    edo = ctr.status()
-    if edo == 'CREATED':
-        ctr.start()
-    elif edo == 'RUNNING':
-        break
-
-print("Directorio de trabajo")
-if ctr.status() == 'RUNNING':
-    r = ctr.execute('ls', '/var/www/html')
-    print(r)
+print("Downloading Image...")
+infraestructura.pullimage(image)
+time.sleep(3)
+print("Image downloaded")
+print("Running container...")
+infraestructura.runcontainerwithport(image, binding)
+time.sleep(3)
+print("Container running with the port binding " + str(binding))
